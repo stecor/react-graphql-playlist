5e3da552b352b8693d72b807da2cefdfbc593a9c
Stefano Corra <stefano.corra@hotmail.com>
2018-05-13 23:37:34 -0400

    rendering data

--- server/app.js
+++ server/app.js
@@ -3,8 +3,12 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require('mongoose');
 const config = require('../config');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 mongoose.connect(config.MONGODB_CONNECTION);
 
