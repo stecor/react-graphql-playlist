f74281041003e40de1db97c6ad2999e166c35baa
Stefano Corra <stefano.corra@hotmail.com>
2018-05-13 22:58:35 -0400

    client components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Booklist from './components/BookList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Ninja Reading</h1>
+        <Booklist />
       </div>
     );
   }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul id="book-list">
+          <li>book name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
