408e698753d21c594e30a3f86c581d54131c4f7b
Stefano Corra <stefano.corra@hotmail.com>
2018-05-28 00:16:19 -0400

    book details

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -13,6 +13,13 @@ import BookDetails from './BookDetails';
 
 class BookList extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
+
   displayBooks(){
     var data = this.props.data;
     if(data.loading){
@@ -20,7 +27,7 @@ class BookList extends Component {
     }else{
       return data.books.map(book =>{
         return(
-          <li key={book.id}>{book.name}</li>
+          <li key={book.id} onClick ={(e)=>{this.setState({selected:book.id})}}>{book.name}</li>
         );
       })
     }
@@ -33,7 +40,7 @@ class BookList extends Component {
         <ul id="book-list">
           {this.displayBooks()}
         </ul>
-        <BookDetails />
+        <BookDetails bookid={this.state.selected}/>
       </div>
     );
   }
